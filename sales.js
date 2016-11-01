de70ae61abede14a21b279dcb8003fea5edd5b96
Zachary Watkins <zwatkins1@mail.csuchico.edu>
2016-10-31 20:27:30 -0700

    Cookies

--- sales.js
+++ sales.js
@@ -17,6 +17,7 @@ function addItem()
   dollars = asCurrency(runningTotal);
   console.log(dollars);
   document.getElementById("subtotal").innerHTML=dollars;
+  document.getElementById("price").innerHTML="";
   //IF newItem is not a number
   // THEN show an alert: "Enter price as a number"
   //OTHERWISE,
@@ -36,16 +37,15 @@ function asCurrency(val)
 }
 
 //courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
-function setCookie(cname, cvalue, exdays) {
+function setCookie(preTax, cvalue, exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     var expires = "expires="+d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    document.cookie = preTax + "=" + cvalue + ";" + expires + ";path=/";
 }
 //courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
-function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
+function getCookie(preTax) {
+    preTax = runningTotal;
     for(var i = 0; i < ca.length; i++) {
         var c = ca[i];
         while (c.charAt(0) == ' ') {
